f6763701183a5c62afed8026244f6acd8897d512
nikosiz <nikodemb@student.agh.edu.pl>
2023-01-09 21:42:49 +0100

    added error page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -114,5 +114,31 @@ function displayCart() {
     }
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("search-wrapper");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 onLoadCartNumbers();
 displayCart();
