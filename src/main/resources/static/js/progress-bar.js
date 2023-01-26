2573697b9d5b6438e77d2c305c70a235084791b4
yanamis <ymishula@student.agh.edu.pl>
2023-01-27 00:56:31 +0100

    updated progress bar, some changes

--- src/main/resources/static/js/progress-bar.js
+++ src/main/resources/static/js/progress-bar.js
@@ -4,7 +4,8 @@ function move() {
         i = 1;
         var elem = document.getElementById("myBar");
         var width = 1;
-        var id = setInterval(frame, 1000);
+        // ono niby wykorzystuje ilość pól i jest wolniej jak ich jest więcej ale zrobione bardzo na oko
+        var id = setInterval(frame, fieldCount * 250);
         function frame() {
             if (width >= 100) {
                 clearInterval(id);
